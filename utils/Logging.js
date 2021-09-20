f9c6b8d517c9e4f0092a570389909a69d37877ec
Amon Peres <61546395+amonrperes@users.noreply.github.com>
2021-09-20 12:22:47 -0300

    Update Logging.js

--- utils/Logging.js
+++ utils/Logging.js
@@ -2,13 +2,7 @@ class Logging{
   executionLog(func){
     const date = new Date();
 
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    const currentTime = `${hour}:${minutes}:${seconds}`;
-
-    const log = `${currentTime} -> Executing '${func}' function.`
+    const log = `${date} -> Executing '${func}' function.`
     console.log(log);
   }
 }
